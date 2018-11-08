6dbe3c88f78a09c0cf4bcb89a15f64450533dcc7
TMDeater <dannyng10231996@gmail.com>
2018-11-09 00:48:57 +0800

    Implemented Check-in function

--- app/src/main/java/com/cognizant/elt/annualdinner/QRScan.java
+++ app/src/main/java/com/cognizant/elt/annualdinner/QRScan.java
@@ -1,28 +1,34 @@
 package com.cognizant.elt.annualdinner;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.os.Vibrator;
-import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.Detector;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class QRScan extends AppCompatActivity {
     SurfaceView surfaceView;
@@ -30,9 +36,8 @@ public class QRScan extends AppCompatActivity {
     private BarcodeDetector barcodeDetector;
     private CameraSource cameraSource;
     private static final int REQUEST_CAMERA_PERMISSION = 201;
-    Button btnAction;
-    String intentData = "";
-    boolean isEmail = false;
+    String textData = "";
+    String token;
 
 
     @Override
@@ -41,28 +46,14 @@ public class QRScan extends AppCompatActivity {
         setContentView(R.layout.activity_qrscan);
 
         initViews();
+
+        Bundle bundle = getIntent().getExtras();
+        token = bundle.getString("token");
     }
 
     private void initViews() {
         txtBarcodeValue = findViewById(R.id.txtBarcodeValue);
         surfaceView = findViewById(R.id.surfaceView);
-
-
-//        btnAction.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//                if (intentData.length() > 0) {
-//                    if (isEmail)
-//                        startActivity(new Intent(QRScan.this, EmailActivity.class).putExtra("email_address", intentData));
-//                    else {
-//                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(intentData)));
-//                    }
-//                }
-//
-//
-//            }
-//        });
     }
 
     private void initialiseDetectorsAndSources() {
@@ -114,7 +105,7 @@ public class QRScan extends AppCompatActivity {
             }
 
             @Override
-            public void receiveDetections(Detector.Detections<Barcode> detections) {
+            public void receiveDetections(final Detector.Detections<Barcode> detections) {
                 final SparseArray<Barcode> barcodes = detections.getDetectedItems();
                 if (barcodes.size() != 0) {
 
@@ -124,16 +115,66 @@ public class QRScan extends AppCompatActivity {
                         @Override
                         public void run() {
 
-                            if (barcodes.valueAt(0).email != null) {
+                            if (barcodes.valueAt(0).displayValue != null) {
                                 txtBarcodeValue.removeCallbacks(null);
-                                intentData = barcodes.valueAt(0).email.address;
-                                txtBarcodeValue.setText(intentData);
-                                isEmail = true;
-                            } else {
-                                isEmail = false;
-                                intentData = barcodes.valueAt(0).displayValue;
-                                txtBarcodeValue.setText(intentData);
+                                textData = barcodes.valueAt(0).displayValue;
+                                txtBarcodeValue.setText(textData);
+                                cameraSource.stop();
 
+
+                                RequestQueue queue = Volley.newRequestQueue(QRScan.this);
+                                String url = "http://ad-backend.fqs3taypzi.ap-southeast-1.elasticbeanstalk.com/api/checkin/1/"+textData;
+                                StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                                        new com.android.volley.Response.Listener<String>() {
+                                            @Override
+                                            public void onResponse(String response) {
+                                                // response
+                                                Log.d("Response", response);
+                                                JSONObject reader = null;
+                                                try {
+                                                    reader = new JSONObject(response);
+                                                    String isSuccess = reader.getString("success");
+                                                    if (isSuccess.equals("true")) {
+                                                        String name = reader.getString("name");
+                                                        String username = reader.getString("username");
+                                                        String display = name+"\n"+username;
+                                                        txtBarcodeValue.setText(display);
+                                                    } else {
+                                                        Toast toast = Toast.makeText(getApplicationContext(),
+                                                                "Incorrect username or password",
+                                                                Toast.LENGTH_SHORT);
+                                                        toast.show();
+                                                    }
+                                                } catch (JSONException e) {
+                                                    e.printStackTrace();
+                                                }
+
+                                            }
+                                        },
+                                        new com.android.volley.Response.ErrorListener() {
+                                            @Override
+                                            public void onErrorResponse(VolleyError error) {
+                                                // error
+                                                Log.d("Error.Response", error.toString());
+                                                Toast toast = Toast.makeText(getApplicationContext(),
+                                                        "Could not get server response",
+                                                        Toast.LENGTH_SHORT);
+                                                toast.show();
+                                            }
+                                        }
+                                ) {
+                                    @Override
+                                    public Map<String, String> getHeaders()
+                                    {
+                                        Map<String, String>  params = new HashMap<String, String>();
+                                        params.put("Authorization", token);
+
+                                        return params;
+                                    }
+                                };
+                                queue.add(postRequest);
+                            } else {
+                                //barcode value is null
                             }
                         }
                     });
